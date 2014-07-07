4d61211b873ff9b4d8633b501edf3cb019a5e423
alejandro.cabezas05 <alejandro.cabezas05@gmail.com>
2014-07-07 14:45:56 +0000

    +vista login de la aplicacion

---
+++ gri_loncomilla/application/views/Por_defecto/login.php
@@ -0,0 +1,13 @@
+<script>
+    $("#conectar").button().click(function() {
+        conectar();
+    });
+</script>
+    
+<div id="login"> 
+    <input class="ui-corner-all" placeholder="Usuario" size="30" id="user" maxlength="8" required/><br> 
+    <input class="ui-corner-all" placeholder="Contraseña" type="password" size="30" id="pass" required/><br> 
+    <button id="conectar">Conectar</button> 
+</div>
+
+
