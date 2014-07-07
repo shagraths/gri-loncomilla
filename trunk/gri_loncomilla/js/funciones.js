da5168a1604c6063396bbdd102fd1f832399bff2
alejandro.cabezas05@gmail.com <alejandro.cabezas05@gmail.com@78752a86-7f17-f7ea-58eb-5778d48f644a>
2014-07-07 14:41:22 +0000

    + se puede agregar usuario-tecnico-servicio con sus respectivas grillas

--- trunk/gri_loncomilla/js/funciones.js
+++ trunk/gri_loncomilla/js/funciones.js
@@ -13,14 +13,14 @@ function verificarLogin() {
                 if (datos.valor == 1) {
                     $("#cerrar_session").show();
                     $("#login").hide('fast');
-                    if (datos.nivel == 0) {//administrador
+                    if (datos.nivel == "ADMINISTRADOR") {//administrador
                         $.post(
                                 base_url + "welcome/tabla_reserva", {}, function(ruta) {
                             $("#contenido").html(ruta);
                         }
                         );
                     } else {//call center
-                        if (datos.nivel == 1) {
+                        if (datos.nivel == "CALL_CENTER") {
                             $.post(
                                     base_url + "welcome/vendedor", {}, function(ruta) {
                                 $("#contenido").html(ruta);
@@ -68,14 +68,14 @@ function conectar() {
         function(datos) {
             if (datos.valor == 1) {
                 $("#cerrar_session").show('slow');
-                if (datos.nivel == 0) {//administrador
+                if (datos.nivel == "ADMINISTRADOR") {//administrador
                     $.post(
                             base_url + "welcome/tabla_reserva", {}, function(ruta) {
                         $("#contenido").html(ruta);
                     }
                     );
                 } else {//vendedor
-                    if (datos.nivel == 1) {
+                    if (datos.nivel == "CALL_CENTER") {
                         $.post(
                                 base_url + "welcome/vendedor", {}, function(ruta) {
                             $("#contenido").html(ruta);
@@ -97,4 +97,170 @@ function conectar() {
                 'json'
                 );
     }
+}
+function bloquear_id() {
+    $("#id_tec").attr("readonly", true);
+    $("#id_tec").css("background", "lightseagreen");
+    $("#id_s").attr("readonly", true);
+    $("#id_s").css("background", "lightseagreen");
+}
+function guardar_reserva() {
+
+}
+function grillas_admin(){
+    grilla_s();
+    grilla_tec();
+    grilla_u();
+}
+//todo tecnico
+function guardar_tec() {
+    var nombre = $("#nombre_tec").val();
+    var empresa = $("#empresa_tec").val();
+    var estado = $("#estado_tec").val();
+    if (nombre == '' || empresa == '' || estado == 'SELECCIONE') {
+        alert("Faltan datos por completar");
+    } else {
+        $.post(
+                base_url + "welcome/guardar_tec",
+                {nombre: nombre, empresa: empresa , estado: estado},
+        function(datos) {
+            if (datos.valor == 1) {
+                alert("error al registrar");
+            } else {
+                alert("datos almacinados correctamente");
+                $("#nombre_tec").val("");
+                $("#empresa_tec").val("");
+                $("#estado_tec").val("SELECCIONE");
+                grilla_tec();
+                $("#actualizar_tec").button("disable");
+                $("#guardar_tec").button("enable");
+            }
+        }, 'json'
+                );
+    }
+}
+function grilla_tec() {
+    $.post(
+            base_url + "welcome/grilla_tec",
+            {},
+            function(ruta, datos) {
+                $("#grilla_tec").hide();
+                $("#grilla_tec").html(ruta, datos);
+                $("#grilla_tec").show('slow');
+            }
+    );
+}
+function actualizar_tec() {
+    var nombre = $("#nombre_tec").val();
+    var empresa = $("#empresa_tec").val();
+    var estado = $("#estado_tec").val();
+    if (nombre == '' || empresa == '' || estado == 'SELECCIONE') {
+        alert("Faltan datos por completar");
+    } else {
+        $.post(
+                base_url + "welcome/guardar_tec",
+                {nombre: nombre, empresa: empresa , estado: estado},
+        function(datos) {
+            if (datos.valor == 1) {
+                alert("error al registrar");
+            } else {
+                alert("datos almacinados correctamente");
+                $("#nombre_tec").val("");
+                $("#empresa_tec").val("");
+                $("#estado_tec").val("SELECCIONE");
+                grilla_tec();
+                $("#actualizar_tec").button("disable");
+                $("#guardar_tec").button("enable");
+            }
+        }, 'json'
+                );
+    }
+}
+function eliminar_tec(id) {
+    $.post(
+            base_url + "welcome/eliminar_tec", {id: id}
+    );
+    grilla_tec();
+}
+//todo servicio
+function guardar_s() {
+    var nombre = $("#nombre_s").val();
+    var estado = $("#estado_s").val();
+    if (nombre == '' || estado == 'SELECCIONE') {
+        alert("Faltan datos por completar");
+    } else {
+        $.post(
+                base_url + "welcome/guardar_s",
+                {nombre: nombre, estado: estado},
+        function(datos) {
+            if (datos.valor == 1) {
+                alert("error al registrar");
+            } else {
+                alert("datos almacinados correctamente");
+
+                $("#nombre_s").val("");                
+                $("#estado_s").val("SELECCIONE");
+                grilla_s();
+                $("#actualizar_s").button("disable");
+                $("#guardar_s").button("enable");
+            }
+        }, 'json'
+                );
+    }
+}
+function grilla_s() {
+    $.post(
+            base_url + "welcome/grilla_s",
+            {},
+            function(ruta, datos) {
+                $("#grilla_s").hide();
+                $("#grilla_s").html(ruta, datos);
+                $("#grilla_s").show('slow');
+            }
+    );
+}
+//todo usuario
+function guardar_u() {
+    var rut = $("#rut").val();
+    var nombre = $("#nombre_u").val();
+    var apellido = $("#apellido").val();
+    var clave = $("#clave").val();
+    var tipo = $("#tipo_u").val();
+    var estado = $("#estado_u").val();
+    if ( rut=='' || nombre == '' || apellido == '' || clave=='' ||  tipo == 'SELECCIONE' || estado == 'SELECCIONE') {
+        alert("Faltan datos por completar");
+    } else {
+        $.post(
+                base_url + "welcome/guardar_u",
+                { rut:rut, nombre: nombre, apellido:apellido, clave:clave, tipo:tipo , estado: estado},
+        function(datos) {
+            if (datos.valor == 1) {
+                alert("error al registrar");
+            } else {
+                alert("datos almacinados correctamente");
+                $("#rut").val("");
+                $("#nombre_u").val("");
+                $("#apellido").val("");
+                $("#clave").val("");
+                $("#tipo_u").val("SELECCIONE");
+                $("#estado_u").val("SELECCIONE");
+                grilla_u();
+                $("#actualizar_u").button("disable");
+                $("#guardar_u").button("enable");
+            }
+        }, 'json'
+                );
+
+    }
+}
+function grilla_u() {
+    $.post(
+            base_url + "welcome/grilla_u",
+            {},
+            function(ruta, datos) {
+                $("#grilla_u").hide();
+                $("#grilla_u").html(ruta, datos);
+                $("#grilla_u").show('slow');
+            }
+    );
 }
