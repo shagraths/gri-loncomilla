134d70ceb58a45a72e6e64bacc821717d027fc51
alejandro.cabezas05@gmail.com <alejandro.cabezas05@gmail.com@78752a86-7f17-f7ea-58eb-5778d48f644a>
2014-07-07 14:47:23 +0000

    +vista header y footer, contiene un div llamado contenido para ir haciendo el cambio de forma dinamica entre las vistas

---
+++ trunk/gri_loncomilla/application/views/Por_defecto/home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <script type="text/javascript" src="<?= base_url() ?>../js/jquery-1.10.2.js"></script>
+        <script type="text/javascript" src="<?= base_url() ?>../js/jquery-ui-1.10.4.js"></script>
+        <link type="text/css" rel="stylesheet" href="<?= base_url(); ?>../css/jquery-ui-1.10.4.css">
+        <link type="text/css" rel="stylesheet"href="<?= base_url(); ?>../css/estilo.css"/>
+        <script type="text/javascript" src="<?= base_url() ?>../js/funciones.js"></script>
+
+        <script type="text/javascript">
+            var base_url = '<?= base_url(); ?>';
+        </script>   
+<div id="cerrar_session">
+        <button id="cerrar"> cerrar sesion</button>                                                
+    </div> 
+    <div id="logo1"></div>
+    <div id="titulo"></div>
+    
+</head>
+
+<body>
+    <div id="contenido">
+        
+    </div>       
+</body>
+
+<footer>
+    <div id="logo2"></div>
+</footer>     
+</html>
