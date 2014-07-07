54b1832985777fe28aef07b25fad8968744089bf
alejandro.cabezas05 <alejandro.cabezas05@gmail.com>
2014-07-07 14:45:01 +0000

    + terminada con boton cargar y borrar

---
+++ gri_loncomilla/application/views/ADMINISTRADOR/GRILLAS/grilla_u.php
@@ -0,0 +1,42 @@
+<? if ($cantidad == 0): ?>
+    <label>no registra Dias Festivos!!</label>
+<? else: ?>
+    <table>
+        <thead>
+        <th>RUT</th>
+        <th>Nombre</th>
+        <th>Apellido</th>        
+        <th>Tipo</th>
+        <th>Estado</th>
+        <th>Borrar</th>
+        <th>Cargar</th>
+    </thead>
+    <tbody>
+        <? $i = 0; ?>
+        <?php foreach ($datos as $filas): ?> 
+        <? $i++; ?>
+            <? if ($i % 2 == 0): ?>
+                <tr align="center">
+                    <td><?= $filas->rut; ?></td>
+                    <td><?= $filas->nombre; ?></td>
+                    <td><?= $filas->apellido; ?></td>                    
+                    <td><?= $filas->nivel; ?></td>
+                    <td><?= $filas->estado_us; ?></td>
+                    <td><button onclick="eliminar_u(<?= $filas->rut; ?>)"><span class="ui-icon ui-icon-trash"></span></button></td>
+                    <td><button onclick="cargar_u(<?= $filas->rut; ?>, '<?= $filas->nombre; ?>', '<?= $filas->apellido; ?>', '<?= $filas->pass; ?>','<?= $filas->nivel; ?>','<?= $filas->estado_us; ?>')"><span class="ui-icon ui-icon-circle-arrow-n"></span></button></td>
+                </tr>
+            <? else: ?>
+                <tr align="center" class="alt">
+                    <td><?= $filas->rut; ?></td>
+                    <td><?= $filas->nombre; ?></td>
+                    <td><?= $filas->apellido; ?></td>                    
+                    <td><?= $filas->nivel; ?></td>
+                    <td><?= $filas->estado_us; ?></td>
+                    <td><button onclick="eliminar_u(<?= $filas->rut; ?>)"><span class="ui-icon ui-icon-trash"></span></button></td>
+                    <td><button onclick="cargar_u(<?= $filas->rut; ?>, '<?= $filas->nombre; ?>', '<?= $filas->apellido; ?>', '<?= $filas->pass; ?>','<?= $filas->nivel; ?>','<?= $filas->estado_us; ?>')"><span class="ui-icon ui-icon-circle-arrow-n"></span></button></td>
+                </tr>
+            <? endif; ?>
+        <?php endforeach; ?>
+    </tbody>
+    </table>
+<? endif; ?>
